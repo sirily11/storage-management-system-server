0cffcd362753a2eb970d6bc9273a4681d8777ce6
sirily11 <sirily1997@gmail.com>
2020-07-05 16:37:53 -0500

    added reactapp

---
+++ serverless/reactapp/build/precache-manifest.75c77d12a7ec402fe7cab969d623407b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5441ccdf5fd23064b9c10858bbf7a3f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2e562447c1901e9c999",
+    "url": "/static/css/2.01b93215.chunk.css"
+  },
+  {
+    "revision": "bf0ae2011cb4bae00849",
+    "url": "/static/css/main.2ab3a0f0.chunk.css"
+  },
+  {
+    "revision": "d2e562447c1901e9c999",
+    "url": "/static/js/2.5db7fc02.chunk.js"
+  },
+  {
+    "revision": "92f320287b4a6131c85a8034a67626e7",
+    "url": "/static/js/2.5db7fc02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf0ae2011cb4bae00849",
+    "url": "/static/js/main.2452788d.chunk.js"
+  },
+  {
+    "revision": "4b35d9a4242a0b4f4871",
+    "url": "/static/js/runtime-main.f5ce99a8.js"
+  }
+]);
